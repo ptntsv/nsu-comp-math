99de73eb9520ea512c4bcc738d7d8cd3e0aa335d
ptntsv <hitogavax@ya.ru>
2025-05-07 11:03:37 +0700

    Task5

--- task5/splines.cpp
+++ task5/splines.cpp
@@ -25,50 +25,61 @@ vector<double> thomas(vector<double>& A, vector<double>& B, vector<double>& C,
                       vector<double>& D) {
     int m = A.size();
     vector<double> alpha(m), beta(m);
-    double k1 = -C[0], k2 = -A[m - 1];
-    alpha[0] = k1;
-    beta[0] = D[0];
+    alpha[0] = -C[0] / B[0];
+    beta[0] = D[0] / B[0];
 
-    for (size_t i = 0; i < m - 2; i++) {
-        alpha[i + 1] = -C[i] / (A[i] * alpha[i] + B[i]);
-        beta[i + 1] = (D[i] - A[i] * beta[i]) / -(B[i] + A[i] * alpha[i]);
+    for (size_t i = 1; i < m; i++) {
+        double y = B[i] + A[i] * alpha[i - 1];
+        alpha[i] = -C[i] / y;
+        beta[i] = (D[i] - A[i] * beta[i - 1]) / y;
     }
 
-    vector<double> roots(m);
-    roots[m - 1] = (D[m - 1] + k2 * beta[m - 1]) / (1 - k2 * alpha[m - 1]);
+    vector<double> coefficients(m);
+    coefficients[m - 1] = beta[m - 1];
 
     for (int i = m - 2; i >= 0; --i) {
-        roots[i] = alpha[i + 1] * roots[i + 1] + beta[i + 1];
+        coefficients[i] = alpha[i] * coefficients[i + 1] + beta[i];
     }
 
-    return roots;
+    return coefficients;
 }
 
 vector<double> get_spline_coefficients(vector<double>& x, vector<double>& y,
                                        double c0, double cn) {
     int n = x.size() - 1;  // number of segments
     vector<double> h(n);
-    int m = n + 1;  // SLE matrix dimension
+    int m = n - 1;  // SLE matrix dimension
     for (size_t i = 0; i < n; i++) h[i] = x[i + 1] - x[i];
     vector<double> A(m), B(m), C(m), D(m);
 
-    B[0] = 1;
-    C[0] = 0;
-    D[0] = c0;
-
-    A[m - 1] = 0;
-    B[m - 1] = 1;
-    D[m - 1] = cn;
-
-    for (size_t i = 1; i < m - 1; i++) {
-        A[i] = h[i - 1] / 6;
-        B[i] = (h[i - 1] + h[i]) / 3;
-        C[i] = h[i] / 6;
-        D[i] = (y[i + 1] - y[i]) / h[i] - (y[i] - y[i - 1]) / h[i - 1];
+    for (size_t i = 0; i < m; i++) {
+        if (i == 0) {
+            B[i] = 2 * (h[0] + h[1]);
+            C[i] = h[1];
+            D[i] =
+                6 * ((y[2] - y[1]) / h[1] - (y[1] - y[0]) / h[0]) - h[0] * c0;
+        } else if (i == m - 1) {
+            A[i] = h[i];
+            B[i] = 2 * (h[i] + h[i + 1]);
+            D[i] = 6 * ((y[i + 2] - y[i + 1]) / h[i + 1] -
+                        (y[i + 1] - y[i]) / h[i]) -
+                   h[i + 1] * cn;
+        } else {
+            A[i] = h[i];
+            B[i] = 2 * (h[i] + h[i + 1]);
+            C[i] = h[i + 1];
+            D[i] = 6 * ((y[i + 2] - y[i + 1]) / h[i + 1] -
+                        (y[i + 1] - y[i]) / h[i]);
+        }
     }
-
     vector<double> c = thomas(A, B, C, D);
-    return c;
+    vector<double> full(n + 1);
+    for (size_t i = 1; i < n; i++) {
+        full[i] = c[i - 1];
+    }
+    full[0] = c0;
+    full[n] = cn;
+    return full;
 }
 
 vector<double> get_spline_coefficients_2(vector<double>& x, vector<double>& y,
@@ -79,14 +90,12 @@ vector<double> get_spline_coefficients_2(vector<double>& x, vector<double>& y,
     vector<double> A(m), B(m), C(m), D(m);
     for (size_t i = 0; i < m; i++) {
         if (i == 0) {
-            A[i] = 0;
             B[i] = h[i] / 3;
             C[i] = h[i] / 6;
             D[i] = (y[1] - y[0]) / h[0] - lCond;
         } else if (i == m - 1) {
             A[i] = h[i - 1] / 6;
             B[i] = h[i - 1] / 3;
-            C[i] = 0;
             D[i] = rCond - (y[i] - y[i - 1]) / h[i - 1];
         } else {
             A[i] = h[i - 1] / 6;
@@ -124,7 +133,7 @@ map<double, double> interpolate(vector<double>& x,
     std::transform(x.begin(), x.end(), y.begin(), f);
     map<double, double> interpolated{};
     int m = x.size() - 1;
-    int k = 10;  // number of points inside segment
+    int k = 50;  // number of points inside segment
     vector<double> h(m);
     vector<double> c(get_spline_coefficients(x, y, c0, cn));
     // vector<double> c(get_spline_coefficients_2(x, y, c0, cn));
@@ -148,7 +157,7 @@ double avg_error(vector<double>& xs, std::function<double(double)> f,
     map<double, double> actual = interpolate(xs, f, lcond, rcond);
     vector<double> errors;
     for (auto const& [x, y] : actual) {
-        cout << "(" << x << ", " << y << ") ";
+        cout << n - 1 << " " << x << " " << std::abs(y - f(x)) << endl;
         errors.push_back(abs(y - f(x)));
     }
     return accumulate(errors.begin(), errors.end(), 0.0) / errors.size();
@@ -157,53 +166,90 @@ double avg_error(vector<double>& xs, std::function<double(double)> f,
 void cubic_spline_demo() {
     double lcond = 0, rcond = 0;
     map<int, double> error_rel{};
-    for (int n = 7; n < 20; n += 2) {
+    for (int n = 5; n < 12; n += 2) {
         vector<double> x(n + 1);
         for (size_t i = 0; i < n + 1; i++) x[i] = 2.0 * i / n - 1;
         error_rel.insert(make_pair(n, avg_error(x, f, lcond, rcond)));
-        return;
     }
-    for (auto const& [n, error] : error_rel) {
-        cout << n << ": " << error << endl;
+    // for (auto const& [n, error] : error_rel) {
+    //     cout << n << ": " << error << endl;
+    // }
+}
+
+void first_row(int order, double value, double y, double h, double& a,
+               double& b, double& c, double& d) {
+    if (order == 0) {
+        b = -2;
+        c = 1;
+        d = h * h * y - value;
+    } else if (order == 1) {
+        b = 1;
+        c = -1;
+        d = -h * (value + h * y);
+    }
+}
+
+void last_row(int order, double value, double y, double h, double& a, double& b,
+              double& c, double& d) {
+    if (order == 0) {
     }
 }
 
 vector<double> solve(const vector<double>& y, pair<int, int> order,
                      pair<double, double> values, double h) {
-    if (order.first != 0 || order.first != 1 || order.second != 0 ||
-        order.second != 1)
-        return vector<double>{};
-
     int m = y.size() - 2;
+    double lcond, rcond;
     vector<double> A(m), B(m), C(m), D(m);
     for (size_t i = 0; i < m; i++) {
         if (i == 0) {
-            A[i] = 0;
+            first_row(order.second, values.first, y[i], h, A[i], B[i], C[i],
+                      D[i]);
+        } else if (i == m - 1) {
+            last_row(order.second, values.second, y[i], h, A[i], B[i], C[i],
+                     D[i]);
+            A[i] = 1;
             B[i] = -2;
             C[i] = 1;
-            D[i] = y[i] * h * h;
+            D[i] = h * h * y[i] - C[i] * rcond;
+        } else {
+            A[i] = 1;
+            B[i] = -2;
+            C[i] = 1;
+            D[i] = h * h * y[i];
         }
     }
     vector<double> roots = thomas(A, B, C, D);
-    vector<double> full(m + 2);
 
-    int n = A.size();
+    int n = m + 2;
+    vector<double> full(n);
+    for (size_t i = 1; i < full.size(); i++) {
+        full[i] = roots[i - 1];
+    }
+
+    full[0] = lcond;
+    full[n - 1] = rcond;
+
+    return full;
 }
 
 double u(double x) { return cos(x); }
+double u_actual(double x) { return -1 + x * x / 2 - pow(x, 4) / 24; }
 
 void de_demo() {
     double offset = 1e-8;
-    double lcond = 0, rcond = 0;
+    pair<int, int> order{0, 0};
+    pair<double, double> values{1, 2};
     double k = 10;
     double a = -M_PI_2, b = M_PI_2;
-    vector<double> xs = split_segment(a + offset, b, k);
+    vector<double> xs = split_segment(a, b, k);
     double h = (b - a - offset) / k;
     vector<double> ys(xs.size());
     transform(xs.begin(), xs.end(), ys.begin(), u);
-    // prep_coefficients(ys, A, B, C, D, lcond, rcond, h);
-    // vector<double> solution = thomas(A, B, C, D);
-    // println(solution);
+    vector<double> solution = solve(ys, order, values, h);
+    vector<double> u_act(xs.size());
+    transform(xs.begin(), xs.end(), u_act.begin(), u_actual);
+    println(solution);
+    println(u_act);
 }
 
 int main() { cubic_spline_demo(); }
--- task5/test.py
+++ task5/test.py
@@ -1,5 +1,10 @@
 from scipy.interpolate import CubicSpline
 import numpy as np
+import matplotlib.pyplot as plt
+
+_, axs = plt.subplots(2)
+axs[0].set_title("Actual")
+axs[1].set_title("Expected")
 
 
 def f(x):
@@ -13,22 +18,18 @@ def split_segment(l, r, n):
 
 def setup1():
     rel = {}
-    for n in range(7, 20, 2):
+    for n in range(5, 13, 2):
         x = [2 * i / n - 1 for i in range(n + 1)]
         y = list(map(f, x))
         cs = CubicSpline(x, y, bc_type=((2, 0), (2, 0)))
-        k = 10
+        k = 50
         xs = list()
         for i in range(1, len(x)):
             xs += split_segment(x[i - 1], x[i], k)
-        print(cs(xs))
-        return
-        # xs.append(x[-1])
-        # actual = cs(xs)
-        # expected = list(map(f, xs))
-        # error = np.average([abs(actual[i] - expected[i]) for i in range(len(actual))])
-        # rel[n] = error
-    print(rel)
+        xs.append(x[-1])
+        actual = cs(xs)
+        expected = list(map(f, xs))
+        axs[1].plot(xs, [abs(actual[i] - expected[i]) for i in range(len(expected))])
 
 
 def setup2():
@@ -39,4 +40,29 @@ def setup2():
     print(cs(xs))
 
 
+def plot():
+    nmap = dict()
+    with open("../result.txt", "r") as file:
+        for line in file:
+            items = line.strip().split(" ")
+            n = int(items[0])
+            x = float(items[1])
+            diff = float(items[2])
+
+            if n not in nmap:
+                nmap[n] = ([], [])
+            nmap[n][0].append(x)
+            nmap[n][1].append(diff)
+
+    for k, v in nmap.items():
+        axs[0].plot(v[0], v[1], label=f"{k}")
+
+    # xs, actual, expected_ys = setup1()
+    # print(xs[0], actual[0], expected_ys[0])
+    # plt.plot(xs, expected_ys, label="Actual")
+    plt.legend(loc="upper right")
+
+
 setup1()
+plot()
+plt.show()
